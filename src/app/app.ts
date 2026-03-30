9c347f41a26224ade504d5a22ff544eba27e2ac5
arjun <arjunap7785@gmail.com>
2026-03-30 12:30:59 +0530

    did binding pracice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test } from './test/test';
+import { FormsModule } from '@angular/forms' ;
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone : true,
+  imports: [Test,FormsModule,CommonModule],
+  // RouterOutlet
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  products : string[] =["Apple","Banana","orange"]
+  selectedIndex : number | null = null
+  deleteItem(index:number){
+    this.products.splice(index,1)
+  }
+
+  selectItem(index:number){
+    this.selectedIndex = index
+  }
   protected readonly title = signal('my-app');
 }
