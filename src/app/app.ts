e78c254522e71f5340137ded4d7a2f204da9be4b
arjun <arjunap7785@gmail.com>
2026-03-31 14:42:18 +0530

    did one page todo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  text  = '';
+  todos : string[] =[];
+
+  addTodo(){
+    this.todos.push(this.text);
+    this.text = '';
+  }
+
+  deleteTodo(index:number){
+    this.todos.splice(index,1)
+  }
+  
   protected readonly title = signal('my-app');
 }
---
+++ src/app/todo-input/todo-input.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input',
+  imports: [],
+  templateUrl: './todo-input.html',
+  styleUrl: './todo-input.css',
+})
+export class TodoInput {}
