3c4edf5414ae3e8174f2f76c409479bb98acad9e
arjun <arjunap7785@gmail.com>
2026-04-01 16:41:45 +0530

    did signup form and added validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Product } from './product/product';
-import { Productdetail } from './productdetail/productdetail';
+
 
 export const routes: Routes = [
-    {path: '',component: Home},
-    {path:'products',component:Product},
-    {path:'product/:id',component:Productdetail}
     
 ] 
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,70 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl  } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterOutlet,RouterLink],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   
-  
+  signupForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]),
+
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).+$/)
+    ]),
+
+    confirmPassword: new FormControl('', Validators.required)
+
+  }, { validators: this.passwordMatchValidator }); // ✅ custom validator
+
+  // 🔥 Custom Validator (VERY IMPORTANT)
+  passwordMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  // ✅ getters
+  get username() {
+    return this.signupForm.get('username');
+  }
+
+  get email() {
+    return this.signupForm.get('email');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value);
+    }
+  }
 }
 
 
